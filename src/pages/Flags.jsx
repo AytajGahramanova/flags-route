cb09d085d3ebaab8e64fe2f7b86fe6d5348b753c
aytac <qehremanova.aytac2003@gmail.com>
2023-11-29 19:16:00 +0400

    task updated

--- src/pages/Flags.jsx
+++ src/pages/Flags.jsx
@@ -4,10 +4,19 @@ import { useNavigate } from "react-router-dom";
 import { Input, Button } from "./styleFlags";
 import "../../src/App.css";
 
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+
 const Flags = () => {
   const [allFlags, setAllFlags] = useState([]);
   const [filteredFlags, setFilteredFlags] = useState([]);
   const [input, setInput] = useState("");
+
+  const [dropdownOpen, setOpenDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setOpenDropdown(!dropdownOpen);
+  };
 
   const navigate = useNavigate();
 
@@ -27,15 +36,68 @@ const Flags = () => {
     setFilteredFlags(filtered);
   }, [input, allFlags]);
 
+
+  const filterDropdown = (selectedRegion) => {
+    setOpenDropdown(!dropdownOpen);
+  
+    if (selectedRegion) {
+      const filtered = allFlags.filter((item) =>
+        item.region.toLowerCase().includes(selectedRegion.toLowerCase())
+      );
+      setFilteredFlags(filtered);
+    }
+  };
+  
+
+
   return (
     <div>
-      <div className="search">
+      <div className="search-filter">
         <Input
           type="text"
           placeholder="Search for a country..."
           value={input}
           onChange={(e) => setInput(e.target.value)}
         ></Input>
+
+        <DropdownButton 
+          id="dropdown-basic-button"
+          title={dropdownOpen ? "Select Region" : "Filter by Region"}
+          show={dropdownOpen}
+          onClick={toggleDropdown}
+        >
+          <div style={{ backgroundColor: "#FFF5C2", width: 111 }}>
+            <Dropdown.Item
+              style={{ textDecoration: "none", color: "#000", padding: 10 }} onClick={() => filterDropdown("Africa")}
+            >
+              Africa
+            </Dropdown.Item>
+            <br />
+            <Dropdown.Item
+              style={{ textDecoration: "none", color: "#000", padding: 10 }} onClick={() => filterDropdown("America")}
+            >
+              America
+            </Dropdown.Item>
+            <br />
+            <Dropdown.Item
+              style={{ textDecoration: "none", color: "#000", padding: 10 }} onClick={() => filterDropdown("Asia")}
+            >
+              Asia
+            </Dropdown.Item>
+            <br />
+            <Dropdown.Item
+              style={{ textDecoration: "none", color: "#000", padding: 10 }} onClick={() => filterDropdown("Europe")}
+            >
+              Europe
+            </Dropdown.Item>
+            <br />
+            <Dropdown.Item
+              style={{ textDecoration: "none", color: "#000", padding: 10 }} onClick={() => filterDropdown("Oceania")}
+            >
+              Oceania
+            </Dropdown.Item>
+          </div>
+        </DropdownButton>
       </div>
 
       <div
@@ -45,6 +107,7 @@ const Flags = () => {
           gap: 10,
           justifyContent: "center",
           margin: 20,
+          marginTop: 40
         }}
       >
         {filteredFlags.map((item, index) => (
